2d270743eda53a2ecbde56f50bbf245ae65fac1b
alfredo-vive <fredyyanez39@yahoo.com>
2023-10-20 21:50:53 +0200

    doc for testing config

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,15 @@ from server.api.helpers import db_seed
 
 @pytest.fixture()
 def app():
+    """This fixture is used for testing and it creates tghe app, the database and seeds it"""
     app = create_app("sqlite://")
     with app.app_context():
         db.create_all()
         db_seed(db)
-
-    print("CREATING DB")
     yield app
 
 
 @pytest.fixture()
 def client(app):
+    """Creates a test client used for testing"""
     return app.test_client()
